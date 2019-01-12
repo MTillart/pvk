f89cea6ba3fcac25be35606df0a78177e8f0b5d2
MTillart <merliti.tillart@khk.ee>
2019-01-12 15:35:00 +0200

    Menüü massiiv

---
+++ menu/index.php
@@ -0,0 +1,40 @@
+<?php
+$menu = array(
+    array(
+        'tuup' => "praed",
+        'nimetus' => "Sealihapada ploomide ja aprikoosiga",
+        'kirjeldus' =>"sealihapada, lisand, salat, leib",
+        'hind' => 2.65
+    ),
+    array(
+        'tuup' => "praed",
+        'nimetus' => "Praetud kanakints",
+        'kirjeldus' =>"praetud kana, lisand, salat, leib",
+        'hind' => 2.50
+    ),
+    array(
+        'tuup' => "supid",
+        'nimetus' => "Rassolnik",
+        'kirjeldus' =>"supp, hapukoor, leib",
+        'hind' => 1.10
+    ),
+    array(
+        'tuup' => "supid",
+        'nimetus' => "Seljanka",
+        'kirjeldus' =>"supp, hapukoor, leib",
+        'hind' => 1.10
+    ),
+    array(
+        'tuup' => "magustoit",
+        'nimetus' => "Kohupiima kreem",
+        'kirjeldus' =>"kohupiim, moos",
+        'hind' => 0.80
+    ),
+    array(
+        'tuup' => "magustoit",
+        'nimetus' => "Kama maius",
+        'kirjeldus' =>"kama, kohupiim, moos",
+        'hind' => 0.60
+    ),
+
+);
