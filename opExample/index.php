63308735b9f38f55846e6db58ba68c00cc2632bd
MTillart <merliti.tillart@khk.ee>
2019-02-06 10:38:27 +0200

    Php liides. Klass ja selle väljastamine

---
+++ opExample/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'test.php';
+
+$hello1 = new test();
+echo '<pre>';
+print_r($hello1);
+echo '</pre>';
---
+++ opExample/test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: merli
+ * Date: 06-Feb-19
+ * Time: 10:30 AM
+ */
+
+class test
+{
+    var $str = '';
+
+}
